cad3c5c541bf8681f1af5245283396db132ac5bc
larissa bouwknegt <l.bouwknegt@st.hanze.nl>
2021-04-16 18:31:48 +0200

    added a properties file and start implementation @value

--- src/main/java/nl/bioinf/minorapplicationdesign/ontpillen/model/webcrawling/FarmacoWebScraper.java
+++ src/main/java/nl/bioinf/minorapplicationdesign/ontpillen/model/webcrawling/FarmacoWebScraper.java
@@ -5,17 +5,31 @@ import nl.bioinf.minorapplicationdesign.ontpillen.model.MedicineDAO.DrugDao;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 
+@Component
+@Configuration
+@PropertySource("classpath:values.properties")
 public class FarmacoWebScraper extends AbstractWebScraper {
+
+    private static String basicUrl;
+
+    @Value("${farmaco.basic.site}")
+    public void setBasicUrl(String basicUrl) {
+        FarmacoWebScraper.basicUrl = basicUrl;
+    }
 
     FarmacoWebScraper(DrugDao drugDao) {
         super(drugDao);
     }
-
 
     @Override
     public void parseHtml() {
@@ -30,15 +44,17 @@ public class FarmacoWebScraper extends AbstractWebScraper {
         return null;
     }
 
+
     private static Document getConnection(String medicine) throws IOException {
-        String basicUrl = "https://www.farmacotherapeutischkompas.nl/bladeren/preparaatteksten/";
+
+//        String basicUrl = "https://www.farmacotherapeut.nl/bladeren/preparaatteksten/";
         String completeUrl = (basicUrl + medicine.charAt(0) + "/" + medicine).toLowerCase(Locale.ROOT);
         Document doc = Jsoup.connect(completeUrl).get();
         return doc;
     }
 
     // FIXME needs to be linked to the data model
-    private static void parseInformation(DrugDao informationStorage, List<String> druglist) throws IOException {
+    private static void parseInformation( List<String> druglist) throws IOException {
         SSLHelper.bypassSSL();
 
         for (String medicine : druglist) {
@@ -51,5 +67,11 @@ public class FarmacoWebScraper extends AbstractWebScraper {
 
         }
 
+    }
+
+    public static void main(String[] args) throws IOException {
+//        DrugDao informationStorage,
+        List drugs = Arrays.asList("Citalopram", "Lorazepam", "Temazepam");
+        parseInformation(drugs);
     }
 }
