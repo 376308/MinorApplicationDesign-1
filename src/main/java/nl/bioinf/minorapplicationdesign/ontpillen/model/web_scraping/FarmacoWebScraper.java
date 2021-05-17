03d5d0ab8d3b0fa8c4070658bc56f64352e00f62
larissa bouwknegt <l.bouwknegt@st.hanze.nl>
2021-05-17 13:48:23 +0200

    to solve merge conflict

--- src/main/java/nl/bioinf/minorapplicationdesign/ontpillen/model/web_scraping/FarmacoWebScraper.java
+++ src/main/java/nl/bioinf/minorapplicationdesign/ontpillen/model/web_scraping/FarmacoWebScraper.java
@@ -79,8 +79,6 @@ public class FarmacoWebScraper implements AbstractWebScraper {
             //≥ gets parsed as a ?, this is a problem with some drugs
             drugsubstance.setSideEffects(sideEffects);
             drugsubstance.setInteractions(interactions);
-            System.out.println(drugDescription);
-
 
             LOGGER.debug("Side effects for drug: " + medicine + " Side effects: " + sideEffects);
             LOGGER.debug("DrugDescription for drug: " + medicine + " Drug description: " + drugDescription);
